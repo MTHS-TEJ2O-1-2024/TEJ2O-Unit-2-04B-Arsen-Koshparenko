45ad39e35748c1b80c45faa4dd8e32af281c6325
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-09-19 16:42:42 -0400

    Temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Sep 2024
+ * This program measures the temperature of the room.
 */
 
-basic.showString('Hello, World!')
+//basic.showString('Hello, World!')
+basic.clearScreen()
+let celsius
+basic.pause(1000)
+
+let temperature = 25 
+input.onButtonPressed(Button.A,function(){
+    input.temperature()
+    basic.showString(" The temperature is")
+    basic.showNumber(temperature)
+})
